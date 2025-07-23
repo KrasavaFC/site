513d8c806e06aab3814dec5defded7b97adf557a
daim43012 <vgatsko.21@gmail.com>
2025-07-24 00:51:10 +0300

    create footer and header as a global components

---
+++ src/components/Footer.svelte
@@ -0,0 +1,114 @@
+<footer class="footer">
+  <div class="box-container">
+    <!-- == Club Information & Social Media Links == -->
+    <div class="footer-item">
+      <!-- Club Logo -->
+      <a class="logo" href="/">
+        <img src="/assets/images/Krasava/krsv.png" alt="logo" />
+      </a>
+
+      <!-- Club Description -->
+      <p>
+        Your Reliable Partner for Passionate Football Excellence and Team
+        Support. Your Trusted Partner for Inspiring Football Achievements.
+      </p>
+
+      <!-- Social Media Links -->
+      <div class="social">
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-linkedin"></i></a>
+      </div>
+    </div>
+
+    <!-- == Contact Info == -->
+    <div class="footer-item">
+      <h2>Contact Info</h2>
+      <div class="info connect">
+        <p><i class="fas fa-phone"></i><span>+123-456-789</span></p>
+        <p><i class="fas fa-phone"></i><span>+111-222-333</span></p>
+        <p>
+          <i class="fas fa-envelope"></i><span class="gmail">abc@gmail.com</span
+          >
+        </p>
+        <p>
+          <i class="fas fa-envelope"></i><span class="gmail">xyz@gmail.com</span
+          >
+        </p>
+        <p>
+          <i class="fas fa-map-marker-alt"></i><span>Karachi, Pakistan</span>
+        </p>
+      </div>
+    </div>
+
+    <!-- == Quick Links == -->
+    <div class="footer-item">
+      <h2>Quick Links</h2>
+      <div class="info links">
+        <p>
+          <i class="fa-solid fa-futbol"></i><a href="./pages/About/About.html"
+            >About</a
+          >
+        </p>
+        <p>
+          <i class="fa-solid fa-futbol"></i><a
+            href="./pages/Pages/Fixtures.html">Fixtures</a
+          >
+        </p>
+        <p>
+          <i class="fa-solid fa-futbol"></i><a href="./pages/Team/Team.html"
+            >Team</a
+          >
+        </p>
+        <p>
+          <i class="fa-solid fa-futbol"></i><a
+            href="./pages/Result/Results.html">Results</a
+          >
+        </p>
+        <p>
+          <i class="fa-solid fa-futbol"></i><a
+            href="./pages/Blog/Blog-Grid.html">Blogs</a
+          >
+        </p>
+        <p>
+          <i class="fa-solid fa-futbol"></i><a
+            href="./pages/Contact/Contact.html">Contact</a
+          >
+        </p>
+      </div>
+    </div>
+
+    <!-- == Newsletter Subscription == -->
+    <div class="footer-item">
+      <h2>Newsletter</h2>
+      <p>
+        Stay connected for the latest updates on special events and exciting
+        moments at our football club.
+      </p>
+      <!-- Newsletter Description -->
+
+      <form class="dark-form">
+        <div class="input-field">
+          <label for="news-email" class="fas fa-envelope"></label>
+          <input
+            type="email"
+            id="news-email"
+            name="email"
+            class="box"
+            placeholder="Your email address"
+            required
+          />
+          <!-- Email Input Field -->
+        </div>
+        <button type="submit" class="btn">Subscribe</button>
+        <!-- Subscribe Button -->
+      </form>
+    </div>
+  </div>
+
+  <!-- == Designer's Name and Copyright == -->
+  <div class="content">
+    <p>Krasava FC 2025 | All rights reserved</p>
+  </div>
+</footer>
---
+++ src/components/Header.svelte
@@ -0,0 +1,218 @@
+<header class="header">
+  <!-- ===== Header-1 Area (Start) ===== -->
+  <div class="header-1">
+    <!-- == Social == -->
+    <ul class="social-contacts">
+      <li><a href="#" class="fab fa-facebook-f"></a></li>
+      <li><a href="#" class="fab fa-twitter"></a></li>
+      <li><a href="#" class="fab fa-instagram"></a></li>
+      <li><a href="#" class="fab fa-linkedin"></a></li>
+    </ul>
+  </div>
+  <!-- ===== Header-1 Area (End) ===== -->
+
+  <!-- ===== Header-2 Area (Start) ===== -->
+  <div class="header-2">
+    <!-- == Logo == -->
+    <a class="logo" href="/">
+      <img src="/assets/images/Krasava/krsv.png" alt="logo" />
+    </a>
+
+    <div class="container">
+      <!-- == Navbar == -->
+      <nav class="navbar">
+        <a class="nav-btn" href="/">home</a>
+
+        <div class="dropdown-menu">
+          <button class="nav-btn">about</button>
+          <div class="dropdown-content">
+            <a href="./pages/About/About.html">about us</a>
+            <a href="./pages/About/Testimonials.html">testimonials</a>
+            <a href="./pages/About/FAQs.html">fAQs</a>
+          </div>
+        </div>
+
+        <div class="dropdown-menu">
+          <button class="nav-btn">Pages</button>
+          <div class="dropdown-content">
+            <a href="./pages/Pages/Point-Table.html">Point Table</a>
+            <a href="./pages/Pages/Fixtures.html">Fixtures</a>
+            <a href="./pages/Pages/Fixture-Single.html">Fixture Single</a>
+            <a href="./pages/Pages/Portfolio.html">Portfolio</a>
+            <a href="./pages/Pages/Portfolio-Single.html">Portfolio Single</a>
+            <a href="./pages/Pages/Sponsors.html">Sponsors</a>
+            <a href="./pages/Pages/Login.html">login</a>
+            <a href="./pages/Pages/Register.html">register</a>
+            <a href="./pages/Pages/Reset-Password.html">forgot Password</a>
+          </div>
+        </div>
+
+        <div class="dropdown-menu">
+          <button class="nav-btn">team</button>
+          <div class="dropdown-content">
+            <a href="./pages/Team/Team.html">team</a>
+            <a href="./pages/Team/Coaches.html">coaches</a>
+            <a href="./pages/Team/Coach-Single.html">coach single</a>
+            <a href="./pages/Team/Players.html">players</a>
+            <a href="./pages/Team/Player-Single.html">player single</a>
+          </div>
+        </div>
+
+        <div class="dropdown-menu">
+          <button class="nav-btn">Result</button>
+          <div class="dropdown-content">
+            <a href="./pages/Result/Results.html">Results</a>
+            <a href="./pages/Result/Result-Single.html">Result Single</a>
+          </div>
+        </div>
+
+        <div class="dropdown-menu">
+          <button class="nav-btn">blog</button>
+          <div class="dropdown-content">
+            <a href="./pages/Blog/Blog-Grid.html">blogs grid</a>
+            <a href="./pages/Blog/Blog-List.html">blogs list</a>
+            <a href="./pages/Blog/Blog-Single.html">blog Single</a>
+          </div>
+        </div>
+
+        <div class="dropdown-menu">
+          <button class="nav-btn">shop</button>
+          <div class="dropdown-content">
+            <a href="./pages/Shop/Shop-Grid.html">shop grid</a>
+            <a href="./pages/Shop/Shop-List.html">shop list</a>
+            <a href="./pages/Shop/Product-Single.html">product Single</a>
+            <a href="./pages/Shop/Wishlist.html">wishlist</a>
+            <a href="./pages/Shop/Cart.html">cart</a>
+            <a href="./pages/Shop/Checkout.html">checkout</a>
+          </div>
+        </div>
+
+        <a class="nav-btn" href="./pages/Contact/Contact.html">contact</a>
+      </nav>
+
+      <!-- == Icon Container == -->
+      <div class="icon-container">
+        <div id="search-btn" class="icon fa-solid fa-magnifying-glass"></div>
+        <!-- Search Icon -->
+        <a href="./pages/Shop/Cart.html"
+          ><i class="icon fa-solid fa-cart-arrow-down"></i></a
+        >
+        <div id="menu-btn" class="icon fa-solid fa-bars-staggered"></div>
+      </div>
+
+      <!-- == Search Container == -->
+      <form class="search-container">
+        <input
+          type="search"
+          id="search-bar"
+          placeholder="search here"
+          required
+        />
+        <!-- Search Input -->
+        <button type="submit"><i class="fas fa-search"></i></button>
+        <!-- Submit Button -->
+      </form>
+    </div>
+  </div>
+  <!-- ===== Header-2 Area (End) ===== -->
+
+  <!-- ===== Mobile Menu Area (Start) ===== -->
+  <div class="mobile-menu">
+    <nav class="mobile-navbar">
+      <div class="nav-link">
+        <div class="main-nav-link">
+          <a class="nav-btn" href="./index.html">home</a>
+        </div>
+      </div>
+
+      <div class="nav-link">
+        <div class="main-nav-link">
+          <div class="nav-btn">about</div>
+          <i class="fas fa-plus"></i>
+        </div>
+        <div class="sub-nav-link">
+          <a href="./pages/About/About.html">about us</a>
+          <a href="./pages/About/Testimonials.html">testimonials</a>
+          <a href="./pages/About/FAQs.html">fAQs</a>
+        </div>
+      </div>
+
+      <div class="nav-link">
+        <div class="main-nav-link">
+          <div class="nav-btn">pages</div>
+          <i class="fas fa-plus"></i>
+        </div>
+        <div class="sub-nav-link">
+          <a href="./pages/Pages/Point-Table.html">Point Table</a>
+          <a href="./pages/Pages/Fixtures.html">Fixtures</a>
+          <a href="./pages/Pages/Fixture-Single.html">Fixture Single</a>
+          <a href="./pages/Pages/Portfolio.html">Portfolio</a>
+          <a href="./pages/Pages/Portfolio-Single.html">Portfolio Single</a>
+          <a href="./pages/Pages/Sponsors.html">Sponsors</a>
+          <a href="./pages/Pages/Login.html">login</a>
+          <a href="./pages/Pages/Register.html">register</a>
+          <a href="./pages/Pages/Reset-Password.html">forgot Password</a>
+        </div>
+      </div>
+
+      <div class="nav-link">
+        <div class="main-nav-link">
+          <div class="nav-btn">team</div>
+          <i class="fas fa-plus"></i>
+        </div>
+        <div class="sub-nav-link">
+          <a href="./pages/Team/Team.html">team</a>
+          <a href="./pages/Team/Coaches.html">coaches</a>
+          <a href="./pages/Team/Coach-Single.html">coach single</a>
+          <a href="./pages/Team/Players.html">players</a>
+          <a href="./pages/Team/Player-Single.html">player single</a>
+        </div>
+      </div>
+
+      <div class="nav-link">
+        <div class="main-nav-link">
+          <div class="nav-btn">Result</div>
+          <i class="fas fa-plus"></i>
+        </div>
+        <div class="sub-nav-link">
+          <a href="./pages/Result/Results.html">Results</a>
+          <a href="./pages/Result/Result-Single.html">Result Single</a>
+        </div>
+      </div>
+
+      <div class="nav-link">
+        <div class="main-nav-link">
+          <div class="nav-btn">blog</div>
+          <i class="fas fa-plus"></i>
+        </div>
+        <div class="sub-nav-link">
+          <a href="./pages/Blog/Blog-Grid.html">blogs grid</a>
+          <a href="./pages/Blog/Blog-List.html">blogs list</a>
+          <a href="./pages/Blog/Blog-Single.html">blog Single</a>
+        </div>
+      </div>
+
+      <div class="nav-link">
+        <div class="main-nav-link">
+          <div class="nav-btn">Shop</div>
+          <i class="fas fa-plus"></i>
+        </div>
+        <div class="sub-nav-link">
+          <a href="./pages/Shop/Shop-Grid.html">shop grid</a>
+          <a href="./pages/Shop/Shop-List.html">shop list</a>
+          <a href="./pages/Shop/Product-Single.html">product Single</a>
+          <a href="./pages/Shop/Wishlist.html">wishlist</a>
+          <a href="./pages/Shop/Cart.html">cart</a>
+          <a href="./pages/Shop/Checkout.html">checkout</a>
+        </div>
+      </div>
+
+      <div class="nav-link">
+        <div class="main-nav-link">
+          <a class="nav-btn" href="./pages/Contact/Contact.html">contact</a>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <!-- ===== Mobile Menu Area (End) ===== -->
+</header>
