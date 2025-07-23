e163f20653200e15bb2b7bef2ab4fc88d21657f8
daim43012 <vgatsko.21@gmail.com>
2025-07-24 01:22:31 +0300

    add testimonials

---
+++ src/components/About/Testimonials.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+  import Footer from "../Footer.svelte";
+  import Header from "../Header.svelte";
+</script>
+
+<!-- ==================== Scroll-Top Area (Start) ==================== -->
+<a href="#" class="scroll-top">
+  <i class="fa-solid fa-arrow-up-long"></i>
+</a>
+<!-- ==================== Scroll-Top Area (End) ==================== -->
+
+<!-- ==================== Header Area (Start) ==================== -->
+<Header />
+<!-- ==================== Header Area (End) ==================== -->
+
+    
+  <!-- ==================== Page-Title (Start) ==================== -->
+  <div class="page-title">
+
+    <div class="title">
+      <h2>testimonials</h2>
+    </div> 
+
+    <div class="link">
+      <a href="../../index.html">Home</a>
+      <i class="fa-solid fa-angles-right"></i>
+      <span class="page">testimonials</span>
+    </div>
+
+  </div>
+  <!-- ==================== Page-Title (End) ==================== -->
+
+  
+
+  <!-- ==================== Testimonials Area (Start) ==================== -->  
+  <section class="testimonial">
+  
+    <!-- Section Heading -->
+    <div class="heading">
+      <h2>Fan <span>Testimonials</span></h2> 
+    </div>
+    
+    <!-- Testimonial Slider Container -->
+    <div class="swiper-container testimonial-slider">
+      <div class="swiper-wrapper">
+  
+        <!-- Testimonial Item 1 -->
+        <div class="swiper-slide testi-item">
+          <img src="../../assets/images/Testimonials/Pic-1.jpg" alt="Author-Pic">
+          <i class="fas fa-quote-left"></i>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis in ea expedita libero error aut itaque quia praesentium? Consectetur nam, maxime atque sit eius corporis voluptate? Exercitationem dolorem consequatur voluptatibus?</p>
+          <div class="text">
+            <h6>Season Ticket Holder</h6>
+            <h4>Chris Turner</h4>
+          </div>
+        </div>
+  
+        <!-- Testimonial Item 2 -->
+        <div class="swiper-slide testi-item">
+          <img src="../../assets/images/Testimonials/Pic-2.jpg" alt="Author-Pic">
+          <i class="fas fa-quote-left"></i>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis in ea expedita libero error aut itaque quia praesentium? Consectetur nam, maxime atque sit eius corporis voluptate? Exercitationem dolorem consequatur voluptatibus?</p>
+          <div class="text">
+            <h6>Supporter</h6>
+            <h4>Amy Roberts</h4>
+          </div>
+        </div>
+  
+        <!-- Testimonial Item 3 -->
+        <div class="swiper-slide testi-item">
+          <img src="../../assets/images/Testimonials/Pic-3.jpg" alt="Author-Pic">
+          <i class="fas fa-quote-left"></i>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis in ea expedita libero error aut itaque quia praesentium? Consectetur nam, maxime atque sit eius corporis voluptate? Exercitationem dolorem consequatur voluptatibus?</p>
+          <div class="text">
+            <h6>Season Ticket Holder</h6>
+            <h4>Jack Thompson</h4>
+          </div>
+        </div>
+  
+        <!-- Testimonial Item 4 -->
+        <div class="swiper-slide testi-item">
+          <img src="../../assets/images/Testimonials/Pic-4.jpg" alt="Author-Pic">
+          <i class="fas fa-quote-left"></i>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis in ea expedita libero error aut itaque quia praesentium? Consectetur nam, maxime atque sit eius corporis voluptate? Exercitationem dolorem consequatur voluptatibus?</p>
+          <div class="text">
+            <h6>Club Member</h6>
+            <h4>Emma Wilson</h4>
+          </div>
+        </div>
+          
+      </div>
+  
+      <!-- Testimonial Pagination -->
+      <div class="swiper-pagination swiper-pagination3"></div>
+  
+    </div>
+      
+  </section>
+  <!-- ==================== Testimonials Area (End) ==================== -->
+
+
+    
+  <!-- ==================== Footer Area (Start) ==================== -->
+<Footer/>
+  <!-- ==================== Footer Area (End) ==================== -->
---
+++ src/routes/testimonials/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import Testimonials from "../../components/About/Testimonials.svelte";
+</script>
+
+<Testimonials />
