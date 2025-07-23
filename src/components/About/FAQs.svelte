6d74053bfb6c2956ef92e189ac21fc01a2ff9766
daim43012 <vgatsko.21@gmail.com>
2025-07-24 01:22:50 +0300

    create FAQ section

---
+++ src/components/About/FAQs.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+  import Footer from "../Footer.svelte";
+  import Header from "../Header.svelte";
+</script>
+
+<!-- ==================== Scroll-Top Area (Start) ==================== -->
+<a href="#" class="scroll-top">
+  <i class="fa-solid fa-arrow-up-long"></i>
+</a>
+<!-- ==================== Scroll-Top Area (End) ==================== -->
+
+<!-- ==================== Header Area (Start) ==================== -->
+<Header />
+<!-- ==================== Header Area (End) ==================== -->
+
+    
+  <!-- ==================== Page-Title (Start) ==================== -->
+  <div class="page-title">
+
+    <div class="title">
+      <h2>FAQs</h2>
+    </div> 
+
+    <div class="link">
+      <a href="../../index.html">Home</a>
+      <i class="fa-solid fa-angles-right"></i>
+      <span class="page">FAQs</span>
+    </div>
+
+  </div>
+  <!-- ==================== Page-Title (End) ==================== -->
+
+
+
+  <!-- ==================== Faqs Area (Start) ==================== -->
+  <section class="faq">
+
+    <div class="box-container">
+  
+      <!-- FAQ Accordion Container -->
+      <div class="accordion-container">
+  
+        <!-- FAQ Accordion 1: Tickets -->
+        <div class="accordion active">
+          <div class="accordion-heading">
+            <i class="fas fa-plus"></i>
+            <h3>How to buy match tickets?</h3>
+          </div>
+          <p class="accordion-content">To purchase tickets, visit our website or contact our office. Online ticketing is available for your convenience. Stay updated on upcoming matches and ticket availability through our official channels.</p>
+        </div>
+  
+        <!-- FAQ Accordion 2: Memberships -->
+        <div class="accordion">
+          <div class="accordion-heading">
+            <i class="fas fa-plus"></i>
+            <h3>Process for club membership?</h3>
+          </div>
+          <p class="accordion-content">Explore membership tiers and benefits on our website. Register online for the preferred membership level. Join our football community and enjoy exclusive perks associated with your chosen membership.</p>
+        </div>
+  
+        <!-- FAQ Accordion 3: Meet Players -->
+        <div class="accordion">
+          <div class="accordion-heading">
+            <i class="fas fa-plus"></i>
+            <h3>Opportunities to meet players?</h3>
+          </div>
+          <p class="accordion-content">Attend special events and meet-and-greets to interact with players. Stay updated on social media and our website for announcements. Connect with the team and create memorable fan experiences.</p>
+        </div>
+  
+        <!-- FAQ Accordion 4: Youth Programs -->
+        <div class="accordion">
+          <div class="accordion-heading">
+            <i class="fas fa-plus"></i>
+            <h3>Youth development programs?</h3>
+          </div>
+          <p class="accordion-content">Explore youth academy and trial opportunities on our website. Our club is dedicated to nurturing young talent. Find details about eligibility and participation criteria for youth programs.</p>
+        </div>
+  
+        <!-- FAQ Accordion 5: Sponsorship -->
+        <div class="accordion">
+          <div class="accordion-heading">
+            <i class="fas fa-plus"></i>
+            <h3>Explore sponsorship opportunities?</h3>
+          </div>
+          <p class="accordion-content">Businesses interested in sponsoring the club can contact our dedicated team. Discover the benefits of collaboration and learn about the process of forming a successful partnership with our club.</p>
+        </div>
+  
+      </div>
+  
+      <!-- FAQ Image -->
+      <div class="image">
+        <img src="../../assets/images/FAQs/FAQs.jpg" alt="FAQs-Image">
+      </div>
+  
+    </div>
+  
+  </section>
+  <!-- ==================== Faqs Area (End) ==================== -->
+
+ 
+    
+  <!-- ==================== Footer Area (Start) ==================== -->
+<Footer/>
+
+  <!-- ==================== Footer Area (End) ==================== -->
+
