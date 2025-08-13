65de361d6cf48eb7f364628b21c3cfad1ce6ff0b
daim43012 <vgatsko.21@gmail.com>
2025-08-13 19:23:32 +0300

    whole fainid page

---
+++ src/components/Pages/FanID.svelte
@@ -0,0 +1,197 @@
+<script lang="ts">
+  import Footer from "../Footer.svelte";
+  import Header from "../Header.svelte";
+</script>
+
+<a href="#" class="scroll-top">
+  <i class="fa-solid fa-arrow-up-long"></i>
+</a>
+
+<Header />
+
+<div class="page-title">
+  <div class="title">
+    <h2>How to create FanID</h2>
+  </div>
+
+  <div class="link">
+    <a href="/">Home</a>
+    <i class="fa-solid fa-angles-right"></i>
+    <span class="page">FanID</span>
+  </div>
+</div>
+
+<section class="about">
+  <div class="box-container">
+    <div class="image">
+      <div>
+        <img
+          src="../../assets/images/Krasava/fanid/fan_id_1.jpg"
+          alt="About-Image1"
+        />
+      </div>
+    </div>
+
+    <div class="content">
+      <div class="heading">
+        <h2>
+          Step <span>1.</span>
+        </h2>
+      </div>
+      <div class="heading">
+        <h2>Go to the Cyprus Sports Organisation website</h2>
+      </div>
+
+      <p>
+        Register on the Cyprus Sports Organisation website if you are new to it.
+        If you have previously applied for a fan ID online, simply log in to
+        your account.
+      </p>
+
+      <a href="https://fancard.cy/auth/register " class="btn">Register FanID</a>
+    </div>
+  </div>
+</section>
+<section class="about">
+  <div class="box-container">
+    <div class="image">
+      <div>
+        <img
+          src="../../assets/images/Krasava/fanid/fan_id_2.jpg"
+          alt="About-Image1"
+        />
+      </div>
+    </div>
+
+    <div class="content">
+      <div class="heading">
+        <h2>
+          Step <span>2.</span>
+        </h2>
+      </div>
+      <div class="heading">
+        <h2>Start creating an application</h2>
+      </div>
+
+      <p>
+        After registration/authorization, click on the New Application button.
+        You will be greeted with instructions for the site - be sure to read all
+        the points and, if you agree with all of them, check the box next to
+        each one.
+      </p>
+    </div>
+  </div>
+</section>
+<section class="about">
+  <div class="box-container">
+    <div class="image">
+      <div>
+        <img
+          src="../../assets/images/Krasava/fanid/fan_id_3.jpg"
+          alt="About-Image1"
+        />
+      </div>
+    </div>
+
+    <div class="content"></div>
+  </div>
+</section>
+<section class="about">
+  <div class="box-container">
+    <div class="image">
+      <div>
+        <img
+          src="../../assets/images/Krasava/fanid/fan_id_4.jpg"
+          alt="About-Image1"
+        />
+      </div>
+    </div>
+
+    <div class="content">
+      <div class="heading">
+        <h2>
+          Step <span>3.</span>
+        </h2>
+      </div>
+      <div class="heading">
+        <h2>Confirm your passport or ID</h2>
+      </div>
+
+      <p>
+        Next, you will be asked to upload a photo of your passport or ID. Follow
+        the instructions that the system offers you.
+      </p>
+      <p>
+        If you live in Cyprus and have an ARC Card, we recommend using it
+        instead of your passport or ID.
+      </p>
+    </div>
+  </div>
+</section>
+<section class="about">
+  <div class="box-container">
+    <div class="image">
+      <div>
+        <img
+          src="../../assets/images/Krasava/fanid/fan_id_5.jpg"
+          alt="About-Image1"
+        />
+      </div>
+    </div>
+
+    <div class="content">
+      <div class="heading">
+        <h2>
+          Step <span>4.</span>
+        </h2>
+      </div>
+      <div class="heading">
+        <h2>Take a selfie</h2>
+      </div>
+
+      <p>
+        After checking your document, you will be asked to undergo biometric
+        authentication to make sure your photo is real. Prepare your front
+        camera and place your face in the frame for facial recognition. Choose a
+        place with good lighting and a neutral background - during the biometric
+        verification, you will need to take a selfie for your fan card.
+      </p>
+    </div>
+  </div>
+</section>
+<section class="about">
+  <div class="box-container">
+    <div class="image">
+      <div>
+        <img
+          src="../../assets/images/Krasava/fanid/fan_id_6.jpg"
+          alt="About-Image1"
+        />
+      </div>
+    </div>
+
+    <div class="content">
+      <div class="heading">
+        <h2>
+          Step <span>5.</span>
+        </h2>
+      </div>
+      <div class="heading">
+        <h2>Specify your place of residence</h2>
+      </div>
+
+      <p>
+        The system will enter basic information about you directly from your
+        passport data. All you have to do is enter your city of residence in
+        Cyprus and your address.
+      </p>
+      <p>
+        Your Fan ID is ready! It will be available in your personal account and
+        via the link in the letter that will be sent to your email. Be sure to
+        download your fan card and don't forget to show it when entering the
+        stadium.
+      </p>
+    </div>
+  </div>
+</section>
+<Footer />
