601dd65e0ab06cdc134ea3976199ce3cb59e8047
daim43012 <vgatsko.21@gmail.com>
2025-08-13 19:25:41 +0300

    modify mobile menu, add fan id link to header, remove comments

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-  import { onMount, onDestroy } from 'svelte';
-  import { browser } from '$app/environment';
+  import { onMount, onDestroy } from "svelte";
+  import { browser } from "$app/environment";
 
   let cartCount = 0;
   let intervalId: NodeJS.Timeout;
 
   async function fetchCartCount() {
     try {
-      const res = await fetch('/api/user/cart');
+      const res = await fetch("/api/user/cart");
       if (res.ok) {
         const data = await res.json();
         cartCount = data.cart.length;
       } else {
-        console.warn('Cart fetch failed:', res.status);
+        console.warn("Cart fetch failed:", res.status);
       }
     } catch (err) {
-      console.error('Cart fetch error:', err);
+      console.error("Cart fetch error:", err);
     }
   }
 
@@ -33,27 +33,21 @@
 </script>
 
 <header class="header">
-  <!-- ===== Header-1 Area (Start) ===== -->
   <div class="header-1">
-    <!-- == Social == -->
     <ul class="social-contacts">
-      <!-- <li><a href="#" class="fab fa-facebook-f"></a></li>
-      <li><a href="#" class="fab fa-twitter"></a></li> -->
-      <li><a href="https://www.instagram.com/krasavafc/" class="fab fa-instagram"></a></li>
-      <!-- <li><a href="#" class="fab fa-linkedin"></a></li> -->
+      <li>
+        <a href="https://www.instagram.com/krasavafc/" class="fab fa-instagram"
+        ></a>
+      </li>
     </ul>
   </div>
-  <!-- ===== Header-1 Area (End) ===== -->
 
-  <!-- ===== Header-2 Area (Start) ===== -->
   <div class="header-2">
-    <!-- == Logo == -->
     <a class="logo" href="/">
       <img src="/assets/images/Krasava/krsv.png" alt="logo" />
     </a>
 
     <div class="container">
-      <!-- == Navbar == -->
       <nav class="navbar">
         <a class="nav-btn" href="/">home</a>
         <a class="nav-btn" href="/about">About</a>
@@ -61,78 +55,11 @@
         <a class="nav-btn" href="/team">Team</a>
         <a class="nav-btn" href="/tickets">Tickets</a>
         <a class="nav-btn" href="/shop/grid">Shop</a>
-
-        <!-- <div class="dropdown-menu">
-          <button class="nav-btn">about</button>
-          <div class="dropdown-content">
-            <a href="/about">about us</a>
-            <a href="/testimonials">testimonials</a>
-            <a href="/faq">fAQs</a>
-          </div>
-        </div> -->
-
-        <!-- <div class="dropdown-menu">
-          <button class="nav-btn">Pages</button>
-          <div class="dropdown-content">
-            <a href="/table">Point Table</a>
-            <a href="/fixtures">Fixtures</a>
-            <a href="/fixture">Fixture Single</a>
-            <a href="/portfolios">Portfolio</a>
-            <a href="/portfolio">Portfolio Single</a>
-            <a href="/sponsors">Sponsors</a>
-            <a href="/login">login</a>
-            <a href="/register">register</a>
-            <a href="/reset-password">forgot Password</a>
-          </div>
-        </div> -->
-
-        <!-- <div class="dropdown-menu">
-          <button class="nav-btn">team</button>
-          <div class="dropdown-content">
-            <a href="/team">team</a>
-            <a href="/team/coaches">coaches</a>
-            <a href="/team/coach">coach single</a>
-            <a href="/team/players">players</a>
-            <a href="/team/player">player single</a>
-          </div>
-        </div> -->
-
-        <!-- <div class="dropdown-menu">
-          <button class="nav-btn">Result</button>
-          <div class="dropdown-content">
-            <a href="/results">Results</a>
-            <a href="/result">Result Single</a>
-          </div>
-        </div>
-
-        <div class="dropdown-menu">
-          <button class="nav-btn">blog</button>
-          <div class="dropdown-content">
-            <a href="./pages/Blog/Blog-Grid.html">blogs grid</a>
-            <a href="./pages/Blog/Blog-List.html">blogs list</a>
-            <a href="./pages/Blog/Blog-Single.html">blog Single</a>
-          </div>
-        </div> -->
-
-        <!-- <div class="dropdown-menu">
-          <button class="nav-btn">shop</button>
-          <div class="dropdown-content">
-            <a href="/shop/grid">shop grid</a>
-            <a href="/shop/list">shop list</a>
-            <a href="/shop/product">product Single</a>
-            <a href="/shop/wishlist">wishlist</a>
-            <a href="/shop/cart">cart</a>
-            <a href="/shop/checkout">checkout</a>
-          </div>
-        </div> -->
-
+        <a class="nav-btn" href="/fanid">FanID</a>
         <a class="nav-btn" href="/contact">contact</a>
       </nav>
 
-      <!-- == Icon Container == -->
       <div class="icon-container">
-        <!-- <div id="search-btn" class="icon fa-solid fa-magnifying-glass"></div> -->
-        <!-- Search Icon -->
         <a href="/shop/checkout" class="cart-icon-wrapper">
           <i class="icon fa-solid fa-cart-arrow-down"></i>
           {#if cartCount > 0}
@@ -147,7 +74,6 @@
         <div id="menu-btn" class="icon fa-solid fa-bars-staggered"></div>
       </div>
 
-      <!-- == Search Container == -->
       <form class="search-container">
         <input
           type="search"
@@ -155,13 +81,10 @@
           placeholder="search here"
           required
         />
-        <!-- Search Input -->
         <button type="submit"><i class="fas fa-search"></i></button>
-        <!-- Submit Button -->
       </form>
     </div>
   </div>
-  <!-- ===== Header-2 Area (End) ===== -->
 
   <!-- ===== Mobile Menu Area (Start) ===== -->
   <div class="mobile-menu">
@@ -171,89 +94,36 @@
           <a class="nav-btn" href="/">home</a>
         </div>
       </div>
-
       <div class="nav-link">
         <div class="main-nav-link">
-          <div class="nav-btn">about</div>
-          <i class="fas fa-plus"></i>
-        </div>
-        <div class="sub-nav-link">
-          <a href="/about">about us</a>
-          <a href="/testimonials">testimonials</a>
-          <a href="/faq">fAQs</a>
+          <a class="nav-btn" href="/about">About</a>
         </div>
       </div>
-
       <div class="nav-link">
         <div class="main-nav-link">
-          <div class="nav-btn">pages</div>
-          <i class="fas fa-plus"></i>
-        </div>
-        <div class="sub-nav-link">
-          <a href="/table">Point Table</a>
-          <a href="/fixtures">Fixtures</a>
-          <a href="/fixture">Fixture Single</a>
-          <a href="/portfolios">Portfolio</a>
-          <a href="/portfolio">Portfolio Single</a>
-          <a href="/sponsors">Sponsors</a>
-          <a href="/login">login</a>
-          <a href="/register">register</a>
-          <a href="/reset-password">forgot Password</a>
+          <a class="nav-btn" href="/academy">Academy</a>
         </div>
       </div>
-
       <div class="nav-link">
         <div class="main-nav-link">
-          <div class="nav-btn">team</div>
-          <i class="fas fa-plus"></i>
-        </div>
-        <div class="sub-nav-link">
-          <a href="/team">team</a>
-          <a href="/team/coaches">coaches</a>
-          <a href="/team/coach">coach single</a>
-          <a href="/team/players">players</a>
-          <a href="/team/player">player single</a>
+          <a class="nav-btn" href="/team">Team</a>
         </div>
       </div>
-
       <div class="nav-link">
         <div class="main-nav-link">
-          <div class="nav-btn">Result</div>
-          <i class="fas fa-plus"></i>
-        </div>
-        <div class="sub-nav-link">
-          <a href="/results">Results</a>
-          <a href="/result">Result Single</a>
+          <a class="nav-btn" href="/tickets">Tickets</a>
         </div>
       </div>
-
-      <!-- <div class="nav-link">
-        <div class="main-nav-link">
-          <div class="nav-btn">blog</div>
-          <i class="fas fa-plus"></i>
-        </div>
-        <div class="sub-nav-link">
-          <a href="./pages/Blog/Blog-Grid.html">blogs grid</a>
-          <a href="./pages/Blog/Blog-List.html">blogs list</a>
-          <a href="./pages/Blog/Blog-Single.html">blog Single</a>
-        </div>
-      </div> -->
-
       <div class="nav-link">
         <div class="main-nav-link">
-          <div class="nav-btn">Shop</div>
-          <i class="fas fa-plus"></i>
-        </div>
-        <div class="sub-nav-link">
-          <a href="/shop/grid">shop grid</a>
-          <a href="/shop/list">shop list</a>
-          <a href="/shop/product">product Single</a>
-          <a href="/shop/wishlist">wishlist</a>
-          <a href="/shop/cart">cart</a>
-          <a href="/shop/checkout">checkout</a>
+          <a class="nav-btn" href="/shop/grid">Shop</a>
         </div>
       </div>
-
+      <div class="nav-link">
+        <div class="main-nav-link">
+          <a class="nav-btn" href="/fanid">FanID</a>
+        </div>
+      </div>
       <div class="nav-link">
         <div class="main-nav-link">
           <a class="nav-btn" href="/contact">contact</a>
